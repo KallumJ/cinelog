abf433a41215f6bb3c344a1736872a761340141d
KallumJ <kallummj@hotmail.com>
2024-07-20 17:35:57 +0100

    📺 Add What's On carousel

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,36 @@ import MediaCarousel from "@/components/MediaCarousel";
 
 export default async function Home() {
   const popularMovies = await tmdb.movies.popular();
+  const popularShows = await tmdb.tvShows.popular();
+
+  const movieTiles = popularMovies.results.map((m) => (
+    <MediaTile
+      key={m.id}
+      posterSrc={tmdb.image.getPosterUrlFromPath(
+        m.poster_path,
+        PosterSize.W780
+      )}
+      title={m.title}
+    />
+  ));
+
+  const showTiles = popularShows.results.map((s) => (
+    <MediaTile
+      key={s.id}
+      posterSrc={tmdb.image.getPosterUrlFromPath(
+        s.poster_path,
+        PosterSize.W780
+      )}
+      title={s.name}
+    />
+  ));
 
   return (
     <section>
       <h1 className="text-4xl font-bold pb-4">Now Showing:</h1>
-      <MediaCarousel
-        mediaTiles={popularMovies.results.map((m) => (
-          <MediaTile
-            key={m.id}
-            posterSrc={tmdb.image.getPosterUrlFromPath(
-              m.poster_path,
-              PosterSize.W780
-            )}
-            title={m.title}
-          />
-        ))}
-      />
+      <MediaCarousel mediaTiles={movieTiles} />
+      <h1 className="text-4xl font-bold py-4">What&apos;s On:</h1>
+      <MediaCarousel mediaTiles={showTiles} />
     </section>
   );
 }
--- components/MediaCarousel.tsx
+++ components/MediaCarousel.tsx
@@ -12,19 +12,23 @@ export default function MediaCarousel({ mediaTiles }: MediaCarouselProps) {
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      items: 6,
+      slidesToSlide: 3
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 6,
+      slidesToSlide: 3
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 4,
+      slidesToSlide: 2
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 2,
+      slidesToSlide: 1
     },
   };
 
