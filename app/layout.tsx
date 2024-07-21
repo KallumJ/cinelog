54d439aae7761797e4f0c28b27e8d5e7bb380b59
KallumJ <kallummj@hotmail.com>
2024-07-21 15:43:01 +0100

    ✍️ Import MUI fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
 
