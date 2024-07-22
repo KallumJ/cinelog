c2028b9951b061d49a406cab4fc5d5b7eb4df4ef
KallumJ <kallummj@hotmail.com>
2024-07-22 23:56:19 +0100

    📃Add Menu items list

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,45 +13,64 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@nextui-org/react";
-import React from "react";
-import MovieIcon from '@mui/icons-material/Movie';
+import React, { useState } from "react";
+import MovieIcon from "@mui/icons-material/Movie";
+import clsx from "clsx";
 
 import { logout } from "../actions/auth_actions";
 
 import { SearchIcon } from "./icons";
 
 interface NavBarProps {
-    isAuthenticated: boolean;
-    email?: string;
-    className?: string;
+  isAuthenticated: boolean;
+  email?: string;
+  className?: string;
 }
 
-export default function NavBar({ isAuthenticated, email, className }: NavBarProps) {
+export default function NavBar({
+  isAuthenticated,
+  email,
+  className,
+}: NavBarProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    { name: "Release Schedule", href: "#" },
+    { name: "Diary", href: "#" },
+    { name: "Watchlist", href: "#" },
+  ];
+
   return (
-    <Navbar isBordered className={className}>
-      <NavbarBrand>
-        <Link color="foreground" href="/">
-          <MovieIcon className="w-12 h-12" />
-          <p className="font-bold text-inherit mx-2 text-xl">Cinelog</p>
-        </Link>
-      </NavbarBrand>
+    <Navbar
+      className={clsx("h-1", className)}
+      disableAnimation={true}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Link color="foreground" href="/">
+            <MovieIcon className="w-12 h-12" />
+            <p className="font-bold text-inherit mx-2 text-xl">Cinelog</p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Release Schedule
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link color="foreground" href="#">
-            Diary
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Watchlist
-          </Link>
-        </NavbarItem>
+        {menuItems.map((m) => (
+          <NavbarItem key={m.name}>
+            <Link color="foreground" href={m.href}>
+              {m.name}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
       <NavbarContent as="div" className="items-center" justify="end">
         <Input
@@ -86,23 +105,36 @@ export default function NavBar({ isAuthenticated, email, className }: NavBarProp
                 <p className="font-semibold">{email}</p>
               </DropdownItem>
               <DropdownItem key="logout" color="danger">
-                <form action={logout}><button className="w-full h-full" type="submit">Log Out</button></form>
+                <form action={logout}>
+                  <button className="w-full h-full" type="submit">
+                    Log Out
+                  </button>
+                </form>
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
         </NavbarContent>
       ) : (
         <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/login">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="/register" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarItem className="hidden lg:flex">
+            <Link href="/login">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="/register" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
       )}
+      <NavbarMenu className="my-6">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="w-full" color="foreground" href={item.href} size="lg">
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
