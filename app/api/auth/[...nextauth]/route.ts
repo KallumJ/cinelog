b454ffbc1f45d40bead62c5e52f461c3a482be91
KallumJ <kallummj@hotmail.com>
2024-08-18 12:48:44 +0100

    🚫 Don't export handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 
-import { authOptions } from "../../../../lib/auth";
+import { authOptions } from "@/lib/auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
