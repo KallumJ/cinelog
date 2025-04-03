d7f7a6784321212671861942e9d89cd3b8b72bad
Kallum <kallummj@hotmail.com>
2025-04-03 23:39:46 +0100

    Tweaks

---
+++ src/lib/components/media/MediaPoster.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { getSrcForPath } from '$lib/tmdb/utils';
+	import { PosterSize } from 'tmdb-ts';
+
+    const { poster_path, title } = $props();
+</script>
+
+<div class="overflow-hidden rounded-lg shadow-lg">
+	<img src={getSrcForPath(poster_path, PosterSize.W780)} alt="{title} Poster" loading="lazy" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,13 @@
 	import { PosterSize } from 'tmdb-ts';
 	import type { HomePageProps } from './+page.server';
 	import { getSrcForPath } from '$lib/tmdb/utils';
+	import MediaPoster from '$lib/components/media/MediaPoster.svelte';
 
 	let { data }: { data: HomePageProps } = $props();
 </script>
 
 <Tabs.Root class="w-full">
-	<Tabs.List class="mx-auto grid w-full grid-cols-2 sm:w-1/2">
+	<Tabs.List class="mx-auto grid w-11/12 grid-cols-2 sm:w-3/4">
 		<Tabs.Trigger value="movies"><span class="mx-3">Movies</span>🎬</Tabs.Trigger>
 		<Tabs.Trigger value="tv"><span class="mx-3">TV</span>📺</Tabs.Trigger>
 	</Tabs.List>
@@ -16,18 +17,16 @@
 	<Tabs.Content value="movies" class="mt-4">
 		<div class="m-4 grid grid-cols-2 gap-4 sm:grid-cols-5 sm:gap-4">
 			{#each data.movies.results as { title, poster_path }}
-				<div class="overflow-hidden rounded-lg shadow-lg">
-					<img
-						src={getSrcForPath(poster_path, PosterSize.W780)}
-						alt="{title} Poster"
-						loading="lazy"
-					/>
-				</div>
+				<MediaPoster {title} {poster_path} />
 			{/each}
 		</div>
 	</Tabs.Content>
 
 	<Tabs.Content value="tv" class="mt-4">
-		<p>Content for TV</p>
+		<div class="m-4 grid grid-cols-2 gap-4 sm:grid-cols-5 sm:gap-4">
+			{#each data.tvShows.results as { name, poster_path }}
+				<MediaPoster title={name} {poster_path} />
+			{/each}
+		</div>
 	</Tabs.Content>
 </Tabs.Root>
