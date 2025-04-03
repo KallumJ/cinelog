666cc17ba95289e77becfd942ea4851b234c46ab
Kallum <kallummj@hotmail.com>
2025-04-03 22:56:58 +0100

    Add Nav Sheet

---
+++ src/lib/components/navigation/NavSheet.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet/index';
+	import { buttonVariants } from '$lib/components/ui/button/index';
+	import Clapperboard from '@lucide/svelte/icons/clapperboard';
+	import Menu from '@lucide/svelte/icons/menu';
+	import DarkModeToggle from '$lib/components/DarkModeToggle.svelte';
+</script>
+
+<span class="m-4 flex items-center gap-4">
+	<span class="flex items-center gap-2">
+		<Clapperboard />
+		<h1 class="text-xl font-bold">Cinelog</h1>
+	</span>
+	<div class="ml-auto">
+		<Sheet.Root>
+			<Sheet.Trigger class={buttonVariants({ variant: 'outline' })}>
+				<Menu />
+			</Sheet.Trigger>
+			<Sheet.Content side="right">
+				<DarkModeToggle />
+			</Sheet.Content>
+		</Sheet.Root>
+	</div>
+</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,33 @@
 <script lang="ts">
-    import * as Tabs from "$lib/components/ui/tabs";
-	import { PosterSize } from "tmdb-ts";
-	import type { HomePageProps } from "./+page.server";
-	import { getSrcForPath } from "$lib/tmdb/utils";
+	import * as Tabs from '$lib/components/ui/tabs';
+	import { PosterSize } from 'tmdb-ts';
+	import type { HomePageProps } from './+page.server';
+	import { getSrcForPath } from '$lib/tmdb/utils';
 
-    let { data }: { data: HomePageProps } = $props();
+	let { data }: { data: HomePageProps } = $props();
 </script>
-  
-  <Tabs.Root class="w-full">
-    <Tabs.List class="grid w-full grid-cols-2">
-      <Tabs.Trigger value="movies"><span class="mx-3">Movies</span>🎬</Tabs.Trigger>
-      <Tabs.Trigger value="tv"><span class="mx-3">TV</span>📺</Tabs.Trigger>
-    </Tabs.List>
-  
-    <Tabs.Content value="movies" class="mt-4">
-      <div class="grid grid-cols-2 gap-2 m-4">
-        {#each data.movies.results as { title, poster_path }}
-        <div class="rounded-lg overflow-hidden shadow-lg">
-          <img
-            src={getSrcForPath(poster_path, PosterSize.W780)}
-            alt="{title} Poster"
-            loading="lazy" 
-            />
-        </div>
-    
-        {/each}
-      </div>
-      
-      </Tabs.Content>
-  
-    <Tabs.Content value="tv" class="mt-4">
-      <p>Content for TV</p>
-    </Tabs.Content>
-  </Tabs.Root>  
+
+<Tabs.Root class="w-full">
+	<Tabs.List class="mx-auto grid w-full grid-cols-2 sm:w-1/2">
+		<Tabs.Trigger value="movies"><span class="mx-3">Movies</span>🎬</Tabs.Trigger>
+		<Tabs.Trigger value="tv"><span class="mx-3">TV</span>📺</Tabs.Trigger>
+	</Tabs.List>
+
+	<Tabs.Content value="movies" class="mt-4">
+		<div class="m-4 grid grid-cols-2 gap-4 sm:grid-cols-5 sm:gap-4">
+			{#each data.movies.results as { title, poster_path }}
+				<div class="overflow-hidden rounded-lg shadow-lg">
+					<img
+						src={getSrcForPath(poster_path, PosterSize.W780)}
+						alt="{title} Poster"
+						loading="lazy"
+					/>
+				</div>
+			{/each}
+		</div>
+	</Tabs.Content>
+
+	<Tabs.Content value="tv" class="mt-4">
+		<p>Content for TV</p>
+	</Tabs.Content>
+</Tabs.Root>
