9a6369db6009ccbbf7aee1fe7f2027388051fcbc
KallumJ <kallummj@hotmail.com>
2025-04-04 19:13:08 +0100

    Add hover animation

--- src/lib/components/media/MediaPoster.svelte
+++ src/lib/components/media/MediaPoster.svelte
@@ -5,6 +5,6 @@
     const { poster_path, title } = $props();
 </script>
 
-<div class="overflow-hidden rounded-lg shadow-lg">
-	<img src={getSrcForPath(poster_path, PosterSize.W780)} alt="{title} Poster" loading="lazy" />
+<div class="overflow-hidden rounded-lg shadow-lg hover:scale-105 transition-all">
+	<a href="#"><img src={getSrcForPath(poster_path, PosterSize.W780)} alt="{title} Poster" loading="lazy" /></a>
 </div>
