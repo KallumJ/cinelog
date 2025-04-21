d88fe645a525d00701ccbc67321c97378de1a5f0
KallumJ <kallummj@hotmail.com>
2025-04-21 10:20:28 +0100

    Update navsheet

--- src/lib/components/navigation/NavSheet.svelte
+++ src/lib/components/navigation/NavSheet.svelte
@@ -10,22 +10,42 @@
 	import { extractInitials } from '$lib/utils';
 
 	const { session }: { session: Session | null } = $props();
+
+	let isOpen = $state(false);
+
+	const navItems = [
+		{
+			href: "/search",
+			displayText: "Search",
+			shouldDisplay: () => true
+		},
+		{
+			href: '/lists',
+			displayText: 'Lists',
+			shouldDisplay: () => !!session
+		},
+		{
+			href: "/attribution",
+			displayText: "Attribution",
+			shouldDisplay: () => true
+		}
+	];
 </script>
 
 <span class="m-4 flex items-center gap-4">
-	<span class="flex items-center gap-2">
+	<a class="flex items-center gap-2" href="/">
 		<Clapperboard size={32} />
 		<h1 class="text-3xl font-bold">Cinelog</h1>
-	</span>
+	</a>
 	<div class="ml-auto">
-		<Sheet.Root>
+		<Sheet.Root bind:open={isOpen}>
 			<Sheet.Trigger class={buttonVariants({ variant: 'outline' })}>
 				<Menu />
 			</Sheet.Trigger>
 			<Sheet.Content side="right">
 				<div class="space-y-4">
 					{#if session}
-						<span class="flex gap-4 items-center">
+						<span class="flex items-center gap-4">
 							<Avatar.Root class="h-16 w-16">
 								<Avatar.Image
 									src={session.user.user_metadata.avatar_url}
@@ -34,23 +54,34 @@
 								<Avatar.Fallback>{extractInitials(session.user.email ?? '')}</Avatar.Fallback>
 							</Avatar.Root>
 							<p class="text-muted-foreground">Signed in as: {session.user.email}</p>
-							
 						</span>
 						<form method="POST" action="/?/signout">
 							<Button type="submit" variant="destructive">Sign out!</Button>
 						</form>
 					{:else}
-						<form method="POST" action="/?/loginWithDiscord">
-							<Button type="submit" class="bg-[#5865F2] text-foreground hover:text-background"
-								>Sign in with Discord</Button
-							>
-						</form>
-						<form method="POST" action="/?/loginWithGitHub">
-							<Button type="submit">Sign in with GitHub</Button>
+						<span class="flex gap-2">
+							<form method="POST" action="/?/loginWithDiscord">
+								<Button type="submit" class="bg-[#5865F2] text-foreground hover:text-background"
+									>Sign in with Discord</Button
+								>
 							</form>
+							<form method="POST" action="/?/loginWithGitHub">
+								<Button type="submit">Sign in with GitHub</Button>
+							</form>
+						</span>
 					{/if}
-					<DarkModeToggle />
+					<ul>
+						{#each navItems as { shouldDisplay, displayText, href }}
+							{#if shouldDisplay()}
+								<li><Button variant="link" class="text-xl -ml-3" {href} onclick={() => (isOpen = false)}>{displayText}</Button></li>
+							{/if}
+						{/each}
+					</ul>
 				</div>
+				<div class="my-4">
+					<DarkModeToggle  />
+				</div>
+				
 			</Sheet.Content>
 		</Sheet.Root>
 	</div>
