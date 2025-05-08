6bca298fd10a8e18202da9e2f8bdd966d2b3e1df
KallumJ <kallummj@hotmail.com>
2025-05-08 21:11:24 +0100

    Finish schedule

--- src/routes/schedule/+page.server.ts
+++ src/routes/schedule/+page.server.ts
@@ -1,11 +1,71 @@
-export async function load({ url: { searchParams }, setHeaders }) {
+import { endOfMonth, format, startOfMonth } from "date-fns";
+import { MediaType, type Media } from "$lib/tmdb/types";
+import { tmdb } from "$lib/tmdb/tmdb";
+import type { Movie, MovieQueryOptions, TV, TvShowQueryOptions } from "tmdb-ts";
+import { parseMediaArray } from "$lib/tmdb/utils";
+import { getMonthFromStringOrToday } from "$lib/utils";
 
-    setHeaders({
+export async function load({ url: { searchParams }, setHeaders }) {
+    const specifiedMonth = searchParams.get("month");
+    const month = getMonthFromStringOrToday(specifiedMonth)
+
+    const movies: Media[] = await getAllResults(month, MediaType.Movie)
+
+    const tv: Media[] = await getAllResults(month, MediaType.Tv);
+    
+    const media = [...movies, ...tv]
+     
+	const schedule = {
+        media,
+        month
+    }
+
+	setHeaders({
 		'cache-control': 'private, max-age=3600'
 	});
-     
+
 	 return {
-        month: searchParams.get("month") ?? ""
+        month: searchParams.get("month") ?? "",
+		schedule
 	 };
 }
 
+async function getAllResults(month: Date, mediaType: MediaType): Promise<Media[]> {
+    const POPULARITY_THRESHOLD = 4;
+
+    let allResults: unknown[] = [];
+    let currentPage = 1;
+    let totalPages = 0;
+
+    const start = format(startOfMonth(month), "dd/MM/yyyy");
+    const end = format(endOfMonth(month), "dd/MM/yyyy");
+
+    do {
+        const options: MovieQueryOptions | TvShowQueryOptions = {
+            "primary_release_date.gte": start,
+            "primary_release_date.lte": end,
+            "first_air_date.gte": start,
+            "first_air_date.lte": end,
+            include_adult: false,
+            with_original_language: "en",
+            sort_by: "popularity.desc",
+            page: currentPage
+        };
+
+        const result = mediaType === MediaType.Movie ? await tmdb.discover.movie(options) : await tmdb.discover.tvShow(options);
+
+        totalPages = result.total_pages;
+
+        allResults = [...allResults, ...result.results.filter((item: Movie | TV) => item.popularity >= POPULARITY_THRESHOLD)];
+
+        // The results are sorted by popularity, so break if we reach a page where they are below our popularity threshold
+        const firstResult = result.results.shift();
+        if (firstResult?.popularity ?? Number.MAX_VALUE < POPULARITY_THRESHOLD) {
+            break;
+        }
+
+        currentPage++;
+    } while (currentPage <= totalPages);
+
+    return parseMediaArray(allResults);
+}
--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,64 +1,42 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	import { badgeVariants } from '$lib/components/ui/badge/badge.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import type { Media } from '$lib/tmdb/types';
-	import { getMonthFromStringOrToday } from '$lib/utils';
-	import LoaderCircle from '@lucide/svelte/icons/loader-circle';
+	import { MediaType } from '$lib/tmdb/types';
+	import { cn, getMonthFromStringOrToday } from '$lib/utils';
 	import MoveLeft from '@lucide/svelte/icons/move-left';
 	import MoveRight from '@lucide/svelte/icons/move-right';
-	import { format, getDaysInMonth, isSameDay, parse, previousDay, startOfMonth, getDate } from 'date-fns';
+	import { format, getDaysInMonth, isSameDay, parse, previousDay, startOfMonth, getDate, lastDayOfMonth, nextDay, subMonths, addMonths } from 'date-fns';
 
 	const { data } = $props();
 
-	let schedule = $state(getSchedule());
-
-	async function getSchedule(): Promise<{ media: Media[]; date: Date }> {
-		const month = data.month;
-		const response = await fetch(`/api/schedule?month=${month}`);
-
-		return await response.json();
-	}
-
+	const schedule = data.schedule;
 	const selectedMonth = getMonthFromStringOrToday(data.month);
 	const daysInMonth = getDaysInMonth(selectedMonth);
 	const monthText = format(selectedMonth, 'MMMM');
 	const yearText = format(selectedMonth, 'yyyy');
+
+	const startOfSelectedMonth = startOfMonth(selectedMonth);
+	const startOfPreviousMonth = subMonths(startOfSelectedMonth, 1);
+	const startOfNextMonth = addMonths(startOfSelectedMonth, 1)
 </script>
 
-{#await schedule}
-	<span class="flex items-center gap-2">
-		<LoaderCircle class="animate-spin" size={36} />
-		<p class="text-4xl text-muted-foreground">Loading...</p>
-	</span>
-{:then schedule}
-	<span class="mb-4 flex gap-4">
-		<h1 class="text-3xl font-bold">{monthText} {yearText}</h1>
-		<Button
-			onclick={() => {
-				let query = new URLSearchParams($page.url.searchParams.toString());
+<span class="mb-4 flex gap-4">
+	<Button href="/schedule" data-sveltekit-reload>Today</Button>
+	<h1 class="text-3xl font-bold">{monthText} {yearText}</h1>
+	<Button href={`/schedule?month=${format(startOfPreviousMonth, 'MM-yyyy')}`} data-sveltekit-reload><MoveLeft /></Button>
+	<Button href={`/schedule?month=${format(startOfNextMonth, "MM-yyyy")}`} data-sveltekit-reload><MoveRight /></Button>
+</span>
 
-				const startOfSelectedMonth = startOfMonth(selectedMonth);
-				const startOfPreviousMonth = startOfMonth(previousDay(startOfSelectedMonth, 1));
-
-				query.set('month', format(startOfPreviousMonth, 'MM-yyyy'));
-
-				goto(`?${query.toString()}`);
-			}}><MoveLeft /></Button
-		>
-		<Button><MoveRight /></Button>
-	</span>
-
-	<div class="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-7">
-		{#each { length: daysInMonth }, i}
-            {@const currentDate = parse(`${i + 1} ${monthText} ${yearText}`, 'dd MMMM yyyy', new Date())}
-			<div class="h-48 w-full border border-white">
-				<h1>{getDate(currentDate)}</h1>
-                
-				{#each schedule.media.filter( (m) => isSameDay(m.initalReleaseDate, currentDate)) as { title }}
-					<p>{title}</p>
-				{/each}
-			</div>
-		{/each}
-	</div>
-{/await}
+<div class="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-7">
+	{#each { length: daysInMonth }, i}
+		{@const currentDate = parse(`${i + 1} ${monthText} ${yearText}`, 'dd MMMM yyyy', new Date())}
+		<div class="min-h-48 w-full border border-muted-foreground flex flex-col">
+			<h1 class="bg-foreground text-background p-1 font-bold mb-2">{getDate(currentDate)}</h1>
+			
+			{#each schedule.media.filter( (m) => isSameDay(m.initalReleaseDate, currentDate)) as { title, type, tmdbId }}
+				{@const emoji = type === MediaType.Movie ? "🎬" : "📺"}
+				<a href={`/${type}/${tmdbId}`} class={cn(badgeVariants({ variant: "secondary" }), "m-1 text-sm rounded-sm overflow-hidden")}>{emoji}{" "}{title}</a>
+			{/each}
+		</div>
+	{/each}
+</div>
